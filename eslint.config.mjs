8ea2ef0b860b0bfb583c47aff3ce7946d73ba267
rouzwelt <rouzweltt@gmail.com>
2025-02-16 23:10:42 +0000

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,24 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import pluginReact from "eslint-plugin-react";
-import prettierConf from 'eslint-config-prettier';
+import globals from 'globals';
+import pluginJs from '@eslint/js';
 import prettier from 'eslint-plugin-prettier';
-import process from "process";
+import pluginReact from 'eslint-plugin-react';
+import prettierConf from 'eslint-config-prettier';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {
     // files to ignore linting
     ignores: [
-      "dist/",
-      "src/assets/",
-      "node_modules/",
-      "tailwind.config.js",
-      "eslint.config.mjs",
+      'dist/',
+      'src/assets/',
+      'node_modules/',
+      'tailwind.config.js',
+      'eslint.config.mjs',
     ],
   },
   {
     // files to include linting
-    files: ["src/**/*.{js,mjs,cjs,jsx}"],
+    files: ['src/**/*.{js,mjs,cjs,jsx}'],
   },
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
@@ -27,33 +26,32 @@ export default [
 	prettierConf,
   {
     plugins: {
-      pluginReact,
+      react: pluginReact,
       prettier,
     },
     settings: {
       react: {
-        version: "detect"
+        version: 'detect'
       }
     },
-    // custom rules
+    // custom lint rules
     rules: {
-      "no-unused-vars": [
-        "error",
-        { varsIgnorePattern: "^_", argsIgnorePattern: "^_" }
-      ],
-      'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+      'no-console': 'off',
 			'no-trailing-spaces': 'error',
-      // 'react/prop-types': 'off', // prop type validation set to off unless it errors for some components, is that desired?
-      "prettier/prettier": [
-        "error",
+      'no-unused-vars': [
+        'error',
+        { varsIgnorePattern: '^_', argsIgnorePattern: '^_' }
+      ],
+      'prettier/prettier': [
+        'error',
         {
           printWidth: 100,
           tabWidth: 2,
           useTabs: false,
           singleQuote: true,
-          plugins: ["prettier-plugin-react", "prettier-plugin-tailwindcss"],
+          plugins: ['prettier-plugin-react', 'prettier-plugin-tailwindcss'],
         }
-      ]
+      ],
     },
   },
 ];

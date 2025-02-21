1de90a9ee6a14a31720a1597ca55635f37c2a79c
Siddharth2207 <siddharthbhoite22@gmail.com>
2025-02-21 22:08:42 +0530

    update solver

--- src/components/RaindexSolverStatus.js
+++ src/components/RaindexSolverStatus.js
@@ -18,72 +18,139 @@ const OrdersTable = ({ orderSolverLogs }) => {
       </thead>
       <tbody>
         {orderSolverLogs?.map((order, index) => (
-          <tr key={index} className="border-t border-gray-300 align-top text-gray-700">
-            <td className="w-[100px] break-words px-4 py-3 text-center align-top text-sm">
-              {order.network}
-            </td>
-            <td className="w-[150px] break-words px-4 py-2 text-center align-top text-blue-500 underline">
-              <a
-                href={getOrderLink(order.orderHash, order.network)}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                {`${order.orderHash.slice(0, 6)}...${order.orderHash.slice(-4)}`}
-              </a>
-            </td>
-            <td className="px-4 py-3 text-center align-top text-sm">
-              <div className="overflow-x-auto">
-                <table className="min-w-full table-auto rounded-lg border border-gray-200 text-left shadow-sm">
-                  <thead className="bg-gray-100 text-sm uppercase text-gray-700">
-                    <tr>
-                      <th className="w-[120px] break-words px-4 py-2">Timestamp</th>
-                      <th className="w-[120px] break-words px-4 py-2">Pair</th>
-                      <th className="w-[140px] break-words px-4 py-2">ioRatio</th>
-                      <th className="w-[140px] break-words px-4 py-2">Market Price</th>
-                      <th className="w-[140px] break-words px-4 py-2">Price Distance</th>
-                      <th className="w-[160px] break-words px-4 py-2">Order Output Amount</th>
-                      <th className="w-[140px] break-words px-4 py-2">Order Status</th>
-                      <th className="w-[200px] break-words px-4 py-2">Order Reason</th>
-                    </tr>
-                  </thead>
-                  <tbody className="divide-y divide-gray-200 bg-white">
-                    {order.pairs?.map((pairItem, pairIndex) => (
-                      <tr key={pairIndex} className="align-top transition hover:bg-gray-50">
-                        <td className="w-[120px] break-words px-4 py-2 align-top">
-                          {formatTimestamp(pairItem.timestamp / 1000)}
-                        </td>
-                        <td className="w-[120px] break-words px-4 py-2 align-top">
-                          {pairItem.pair}
-                        </td>
-                        <td className="w-[140px] break-words px-4 py-2 align-top">
-                          {pairItem.ioRatio}
-                        </td>
-                        <td className="w-[140px] break-words px-4 py-2 align-top">
-                          {pairItem.marketPrice}
-                        </td>
-                        <td className="w-[140px] break-words px-4 py-2 align-top">
-                          {pairItem.ioRatio - pairItem.marketPrice}
-                        </td>
-                        <td className="w-[160px] break-words px-4 py-2 align-top">
-                          {pairItem.maxOutput}
-                        </td>
-                        <td
-                          className={`w-[140px] break-words px-4 py-2 align-top font-semibold ${
-                            pairItem.orderStatus === 'success' ? 'text-green-500' : 'text-red-500'
-                          }`}
-                        >
-                          {pairItem.orderStatus}
-                        </td>
-                        <td className="w-[200px] break-words px-4 py-2 align-top text-gray-500">
-                          {pairItem.orderReason}
-                        </td>
-                      </tr>
-                    ))}
-                  </tbody>
-                </table>
-              </div>
-            </td>
-          </tr>
+          <>
+           {
+            order.orderStatus === 'found opportunity' ? (
+              <tr key={index} className="border-t border-gray-300 align-top text-gray-700">
+                <td className="w-[100px] break-words px-4 py-3 text-center align-top text-sm">
+                  {order.network}
+                </td>
+                <td className="w-[150px] break-words px-4 py-2 text-center align-top text-blue-500 underline">
+                  <a
+                    href={getOrderLink(order.orderHash, order.network)}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {`${order.orderHash.slice(0, 6)}...${order.orderHash.slice(-4)}`}
+                  </a>
+                </td>
+                <td className="px-4 py-3 text-center align-top text-sm">
+                  <div className="overflow-x-auto">
+                    <table className="min-w-full table-auto rounded-lg border border-gray-200 text-left shadow-sm">
+                      <thead className="bg-gray-100 text-sm uppercase text-gray-700">
+                        <tr>
+                          <th className="w-[120px] break-words px-4 py-2">Timestamp</th>
+                          <th className="w-[120px] break-words px-4 py-2">Pair</th>
+                          <th className="w-[140px] break-words px-4 py-2">Order Status</th>
+                          <th className="w-[200px] break-words px-4 py-2">Order Log</th>
+                        </tr>
+                      </thead>
+                      <tbody className="divide-y divide-gray-200 bg-white">
+                        {order.pairs?.map((pairItem, pairIndex) => (
+                          <tr key={pairIndex} className="align-top transition hover:bg-gray-50">
+                            <td className="w-[120px] break-words px-4 py-2 align-top">
+                              {formatTimestamp(pairItem.timestamp / 1000)}
+                            </td>
+                            <td className="w-[120px] break-words px-4 py-2 align-top">
+                              {pairItem.pair}
+                            </td>
+                            <td
+                              className={`w-[140px] break-words px-4 py-2 align-top font-semibold ${
+                                pairItem.orderStatus === 'found opportunity' ? 'text-green-500' : 'text-red-500'
+                              }`}
+                            >
+                              {pairItem.orderStatus}
+                            </td>
+                            <td className="w-[200px] break-words px-4 py-2 align-top text-gray-500">
+                              <a
+                                href={pairItem.orderReason}
+                                target="_blank"
+                                rel="noreferrer"
+                                className="text-blue-600 hover:underline break-all block truncate"
+                                title={pairItem.orderReason} // Shows full URL on hover
+                              >
+                                {pairItem.orderReason}
+                              </a>
+                            </td>
+
+                          </tr>
+                        ))}
+                      </tbody>
+                    </table>
+                  </div>
+                </td>
+              </tr>
+            ) : (
+              <tr key={index} className="border-t border-gray-300 align-top text-gray-700">
+                <td className="w-[100px] break-words px-4 py-3 text-center align-top text-sm">
+                  {order.network}
+                </td>
+                <td className="w-[150px] break-words px-4 py-2 text-center align-top text-blue-500 underline">
+                  <a
+                    href={getOrderLink(order.orderHash, order.network)}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {`${order.orderHash.slice(0, 6)}...${order.orderHash.slice(-4)}`}
+                  </a>
+                </td>
+                <td className="px-4 py-3 text-center align-top text-sm">
+                  <div className="overflow-x-auto">
+                    <table className="min-w-full table-auto rounded-lg border border-gray-200 text-left shadow-sm">
+                      <thead className="bg-gray-100 text-sm uppercase text-gray-700">
+                        <tr>
+                          <th className="w-[120px] break-words px-4 py-2">Timestamp</th>
+                          <th className="w-[120px] break-words px-4 py-2">Pair</th>
+                          <th className="w-[140px] break-words px-4 py-2">ioRatio</th>
+                          <th className="w-[140px] break-words px-4 py-2">Market Price</th>
+                          <th className="w-[140px] break-words px-4 py-2">Price Distance</th>
+                          <th className="w-[160px] break-words px-4 py-2">Order Output Amount</th>
+                          <th className="w-[140px] break-words px-4 py-2">Order Status</th>
+                          <th className="w-[200px] break-words px-4 py-2">Order Log</th>
+                        </tr>
+                      </thead>
+                      <tbody className="divide-y divide-gray-200 bg-white">
+                        {order.pairs?.map((pairItem, pairIndex) => (
+                          <tr key={pairIndex} className="align-top transition hover:bg-gray-50">
+                            <td className="w-[120px] break-words px-4 py-2 align-top">
+                              {formatTimestamp(pairItem.timestamp / 1000)}
+                            </td>
+                            <td className="w-[120px] break-words px-4 py-2 align-top">
+                              {pairItem.pair}
+                            </td>
+                            <td className="w-[140px] break-words px-4 py-2 align-top">
+                              {pairItem.ioRatio}
+                            </td>
+                            <td className="w-[140px] break-words px-4 py-2 align-top">
+                              {pairItem.marketPrice}
+                            </td>
+                            <td className="w-[140px] break-words px-4 py-2 align-top">
+                              {pairItem.ioRatio - pairItem.marketPrice}
+                            </td>
+                            <td className="w-[160px] break-words px-4 py-2 align-top">
+                              {pairItem.maxOutput}
+                            </td>
+                            <td
+                              className={`w-[140px] break-words px-4 py-2 align-top font-semibold ${
+                                pairItem.orderStatus === 'found opportunity' ? 'text-green-500' : 'text-red-500'
+                              }`}
+                            >
+                              {pairItem.orderStatus}
+                            </td>
+                            <td className="w-[200px] break-words px-4 py-2 align-top text-gray-500">
+                              {pairItem.orderReason}
+                            </td>
+                          </tr>
+                        ))}
+                      </tbody>
+                    </table>
+                  </div>
+                </td>
+              </tr>
+            )
+           }
+          </>
+          
         ))}
       </tbody>
     </table>
@@ -91,9 +158,10 @@ const OrdersTable = ({ orderSolverLogs }) => {
 };
 
 const RaindexSolverStatus = () => {
-  // State to manage selected token
   const [selectedToken, setSelectedToken] = useState('');
+  const [selectedOrderHash, setSelectedOrderHash] = useState('');
   const [solverLogs, setSolverLogs] = useState(null);
+  const [filteredSolverLogs, setFilteredSolverLogs] = useState(null);
 
   const [loadingSolverLogs, setLoadingSolverLogs] = useState(false);
 
@@ -104,18 +172,31 @@ const RaindexSolverStatus = () => {
         try {
           const activeOrders = await fetchAndSetData(selectedToken);
           const fetchedSolverLogs = await fetchSolverLogs(activeOrders);
-
           setSolverLogs(fetchedSolverLogs);
+          setFilteredSolverLogs(fetchedSolverLogs)
         } catch (error) {
           console.error('Error fetching solver logs:', error);
         } finally {
-          setLoadingSolverLogs(false);
+          setLoadingSolverLogs(false);  
         }
       };
       fetchData();
     }
     // eslint-disable-next-line
   }, [selectedToken]);
+
+
+  useEffect(() => {
+    if (selectedToken && solverLogs) {
+      if(selectedOrderHash === ''){
+        setFilteredSolverLogs(solverLogs)
+      }else{
+        setFilteredSolverLogs(solverLogs.filter(i => i.orderHash === selectedOrderHash))
+      }
+    }
+    // eslint-disable-next-line
+  }, [selectedOrderHash]);
+
 
   const fetchAndSetData = async (selectedToken) => {
     const network = tokenConfig[selectedToken]?.network;
@@ -125,6 +206,12 @@ const RaindexSolverStatus = () => {
 
   const fetchSolverLogs = async (orders) => {
     orders = orders.filter((i) => i.active);
+    
+    // Filter by selectedOrderHash if it exists
+    if (selectedOrderHash) {
+      orders = orders.filter((order) => order.orderHash === selectedOrderHash);
+    }
+
     const filteredData = [];
     for (let i = 0; i < orders.length; i++) {
       let order = orders[i];
@@ -135,53 +222,91 @@ const RaindexSolverStatus = () => {
         10,
       );
       for (const orderLog of orderLogs) {
-        const hasDetails = orderLog.attemptDetails !== undefined;
-        filteredData.push({
-          network: tokenConfig[selectedToken].network,
-          orderHash: order.orderHash,
-          pair: orderLog.pair,
-          ioRatio: hasDetails ? orderLog.attemptDetails.quote.ratio : '0',
-          maxOutput: hasDetails ? orderLog.attemptDetails.quote.maxOutput : '0',
-          marketPrice: hasDetails ? orderLog.attemptDetails.fullAttempt.marketPrice : '0',
-          orderStatus: hasDetails ? orderLog.status : 'zero max output',
-          orderReason: hasDetails
-            ? orderLog.attemptDetails.fullAttempt.error
-            : 'No attempt details',
-          timestamp: orderLog.timestamp,
-        });
+        if(orderLog.status !== "found opportunity"){
+          const hasDetails = orderLog.attemptDetails !== undefined;
+          filteredData.push({
+            network: tokenConfig[selectedToken].network,
+            orderHash: order.orderHash,
+            pair: orderLog.pair,
+            ioRatio: hasDetails ? orderLog.attemptDetails.quote.ratio : '0',
+            maxOutput: hasDetails ? orderLog.attemptDetails.quote.maxOutput : '0',
+            marketPrice: hasDetails ? orderLog.attemptDetails.fullAttempt.marketPrice : '0',
+            orderStatus: hasDetails ? orderLog.status : 'zero max output',
+            orderReason: hasDetails
+              ? orderLog.attemptDetails.fullAttempt.error
+              : 'No attempt details',
+            timestamp: orderLog.timestamp,
+          });
+        }else if(orderLog.status === "found opportunity"){
+          filteredData.push({
+            network: tokenConfig[selectedToken].network,
+            orderHash: order.orderHash,
+            pair: orderLog.pair,
+            orderStatus: orderLog.status,
+            orderReason: orderLog.attemptDetails !== undefined ? orderLog.attemptDetails.txUrl : '',
+            timestamp: orderLog.timestamp
+          });
+        }
       }
     }
+    
     const groupedData = {};
+    
     for (const item of filteredData) {
-      const {
-        orderHash,
-        pair,
-        ioRatio,
-        maxOutput,
-        marketPrice,
-        network,
-        orderStatus,
-        orderReason,
-        timestamp,
-      } = item;
-      if (!groupedData[orderHash]) {
-        groupedData[orderHash] = { orderHash, network, pairs: [] };
+      const {orderStatus} = item;
+      if(orderStatus === "found opportunity"){
+        const {
+          network,
+          orderHash,
+          pair,
+          orderReason,
+          timestamp,
+        } = item;
+        if (!groupedData[orderHash]) {
+          groupedData[orderHash] = { orderHash, network, orderStatus, pairs: [] };
+        }
+        groupedData[orderHash].pairs.push({
+          pair,
+          orderStatus,
+          orderReason,
+          timestamp,
+        });
+      } else{
+        const {
+          orderHash,
+          pair,
+          ioRatio,
+          maxOutput,
+          marketPrice,
+          network,
+          orderStatus,
+          orderReason,
+          timestamp,
+        } = item;
+        if (!groupedData[orderHash]) {
+          groupedData[orderHash] = { orderHash, network, orderStatus, pairs: [] };
+        }
+        groupedData[orderHash].pairs.push({
+          pair,
+          ioRatio,
+          maxOutput,
+          marketPrice,
+          orderStatus,
+          orderReason,
+          timestamp,
+        });
       }
-      groupedData[orderHash].pairs.push({
-        pair,
-        ioRatio,
-        maxOutput,
-        marketPrice,
-        orderStatus,
-        orderReason,
-        timestamp,
-      });
     }
     return Object.values(groupedData);
   };
 
   const handleTokenChange = (event) => {
     setSelectedToken(event.target.value);
+  };
+
+  // New handler for order hash input
+  const handleOrderHashChange = (event) => {
+    setSelectedOrderHash(event.target.value);
   };
 
   return (
@@ -204,6 +329,18 @@ const RaindexSolverStatus = () => {
               ))}
             </select>
           </div>
+
+          {/* Order Hash Filter */}
+          <div className="flex items-center space-x-2">
+            <span className="text-sm font-medium">Order Hash:</span>
+            <input
+              type="text"
+              placeholder="Enter Order Hash"
+              className="rounded bg-gray-700 p-2 text-sm text-white placeholder-gray-400 focus:outline-none focus:ring focus:ring-indigo-500"
+              value={selectedOrderHash}
+              onChange={handleOrderHashChange}
+            />
+          </div>
         </div>
       </div>
 
@@ -214,7 +351,7 @@ const RaindexSolverStatus = () => {
             <p className="mt-3 text-lg font-medium text-gray-600">Loading...</p>
           </div>
         ) : (
-          <>{solverLogs && <OrdersTable orderSolverLogs={solverLogs} />}</>
+          <>{filteredSolverLogs && <OrdersTable orderSolverLogs={filteredSolverLogs} />}</>
         )}
         <div className="mt-8 rounded-lg bg-gray-100 p-6 text-base text-gray-700">
           <h3 className="mb-4 text-left text-lg font-semibold">Data Sources</h3>
